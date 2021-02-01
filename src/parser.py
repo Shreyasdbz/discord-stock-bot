942326789361532acb750fe5cde67933cb0cd320
Shreyas Sane <shreyas@Vulcan.attlocal.net>
2021-02-01 17:44:50 -0600

    Setup initial flow

---
+++ src/parser.py
@@ -0,0 +1,5 @@
+# Parses the message text and determines which action will be taken
+# @params: message (string)
+# Returns: 
+def getMessageType(message):
+    pass
--- stockBot.py
+++ stockBot.py
@@ -3,18 +3,10 @@ import os
 from dotenv import load_dotenv
 # Custom src
 from src import discordClient
-from src import financeClient
-from src import utils
 
 load_dotenv()
-DISCORD_CLIENT_TOKEN = os.getenv("DISCORD_CLIENT_TOKEN")
+DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
 IEX_TOKEN = os.getenv("IEX_TOKEN")
 
-# All the servers where stockBot is currently active on
-activeGuilds = ["$Moves", "Bread Winners"]
+discordClient.runClient(DISCORD_TOKEN, IEX_TOKEN)
 
-# discordClient.runClient(DISCORD_CLIENT_TOKEN, IEX_TOKEN)
-# fc = financeClient.getFinanceClient(IEX_TOKEN)
-# fc.getCurrentPrice("AAPL")
-
-utils.getMarketHours()
